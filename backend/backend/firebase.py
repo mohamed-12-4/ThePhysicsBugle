5c5564ee724d7d060d7c2b70b6a7765743863aaf
mohamed-12-4 <m12mm.4m2005@gmail.com>
2024-08-10 21:01:30 +0400

    minor update

--- backend/backend/firebase.py
+++ backend/backend/firebase.py
@@ -1,9 +1,17 @@
 # firebase.py
 import firebase_admin
 from firebase_admin import credentials, firestore
-
+import os
+from pathlib import Path
 # Path to your Firebase service account key JSON file
-cred = credentials.Certificate('/etc/secrets/serviceAccountKey.json')
+
+if os.getenv("DEBUG", "True") != "True":
+    cred = credentials.Certificate('/etc/secrets/serviceAccountKey.json')
+
+else:
+    BASE_DIR = Path(__file__).resolve().parent
+    cred = credentials.Certificate(BASE_DIR / '../serviceAccountKey.json')
+
 
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'the-physics-bugle-3375d.appspot.com'
