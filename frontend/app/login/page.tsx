02c9920f33a27097e16e9a25e98af4fff2e71464
mohamed-12-4 <m12mm.4m2005@gmail.com>
2024-08-14 22:43:32 +0400

    Minor updates in auth

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -4,17 +4,26 @@ import Peact, { useState } from 'react'
 import Image from 'next/image'
 import { useRouter } from "next/navigation";
 import { signIn } from 'next-auth/react';
+import React from 'react';
 function Page() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const router = useRouter();
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  if (loading) {
+     return <div>Loading...</div>
+    }
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setLoading(true);
     const result = await signIn("credentials", {
-      email: e.target.email.value,
-      password: e.target.password.value,
+      email,
+      password,
       redirect: false,
     });
+    setLoading(false);
 
     if (result?.error) {
       setError("Invalid email or password");
@@ -24,6 +33,7 @@ function Page() {
     }
   };
   return (
+
     <section className="padding-container flex flex-col gap-20 py-10 pb-0 md:gap-20 lg:py-10 xl:flex-row border-2 border-black ">
       <form onSubmit={handleSubmit}>
         <div className="hero-map " />
@@ -41,6 +51,7 @@ function Page() {
             placeholder=" Enter your email"
             required
             className="bg-gray-80"
+            onChange={(e) => setEmail(e.target.value)}
           ></input>
           <br></br>
           <h2 className="text-lightpurple-10 bold-4 py-2 pb-3 ">Password</h2>
@@ -52,6 +63,7 @@ function Page() {
             style={{ color: "white", height: "30px", width: "600px" }}
             required
             className="bg-gray-80"
+            onChange={(e) => setPassword(e.target.value)}
           ></input>
           <br></br>
           <div className="py-2 flex relative">
--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,60 +1,83 @@
+'use client';
 import { NAV_LINKS } from '@/constants'
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image';
 import Button from './Button';
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Navbar = () => {
-    return(
-        <nav className="flexBetween max-container padding-container relative z-30 py-5 "> 
+    const { data: session } = useSession();
+    return (
+      <nav className="flexBetween max-container padding-container relative z-30 py-5 ">
         {/* //Link to home page */}
-        <div className='relative flexBetween gap-9'>
-        <Link href="/">
-        <Image src ="/logonew.svg" alt="logo" width={170} height={30} />
-        </Link>
-        <ul className="hidden h-full gap-12 lg:flex">
+        <div className="relative flexBetween gap-9">
+          <Link href="/">
+            <Image src="/logonew.svg" alt="logo" width={170} height={30} />
+          </Link>
+          <ul className="hidden h-full gap-12 lg:flex">
             {NAV_LINKS.map((link) => (
-                <Link href={link.href} key={link.key} className='regular-18 text-lightpurple-10 flexCenter cursor-pointer pb-0 transition-all hover:text-purple-500'>
-                    {link.label}
-                </Link>
-            )
-        )
-            }
-        </ul>
+              <Link
+                href={link.href}
+                key={link.key}
+                className="regular-18 text-lightpurple-10 flexCenter cursor-pointer pb-0 transition-all hover:text-purple-500"
+              >
+                {link.label}
+              </Link>
+            ))}
+          </ul>
         </div>
 
-        <div className='lg:flexCenter hidden gap-4'>
+        {session ? (
+          <div className="lg:flexCenter hidden gap-4">
             <Button
-            type="button"
-            title="Log In"
-            variant="btn_black"
-            rounded='rounded-md'
-            customHeight="custom-height"
-            customWidth='custom-width-90'
-            font="font-Lato"
-            newPath='/login'
+              type="button"
+              title="Sign Out"
+              variant="btn_black"
+              rounded="rounded-md"
+              customHeight="custom-height"
+              customWidth="custom-width-90"
+              font="font-Lato"
+              newPath="/login"
+              onClick={() => signOut()}
             />
+
             
-            <Button 
-            type="button"
-            title="Sign Up"
-            variant="btn_dark_purple"
-            rounded='rounded-md'
-            customHeight="custom-height"
-            font="font-Lato"
-            newPath='/signup'
+          </div>
+        ) : (
+          <div className="lg:flexCenter hidden gap-4">
+            <Button
+              type="button"
+              title="Log In"
+              variant="btn_black"
+              rounded="rounded-md"
+              customHeight="custom-height"
+              customWidth="custom-width-90"
+              font="font-Lato"
+              newPath="/login"
             />
-        </div>
+
+            <Button
+              type="button"
+              title="Sign Up"
+              variant="btn_dark_purple"
+              rounded="rounded-md"
+              customHeight="custom-height"
+              font="font-Lato"
+              newPath="/signup"
+            />
+          </div>
+        )}
 
         <Image
-            src = "/menubar.png" 
-            alt ="menu"
-            width={32}
-            height ={32}
-            className="inline-block cursor-pointer lg:hidden"
-            />
-        </nav>
-    )
+          src="/menubar.png"
+          alt="menu"
+          width={32}
+          height={32}
+          className="inline-block cursor-pointer lg:hidden"
+        />
+      </nav>
+    );
 }
 
 export default Navbar
